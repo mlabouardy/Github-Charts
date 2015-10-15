ec53762b8129f443a231458b32901e734503be0a
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-15 21:45:29 +0200

    created app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var githubApp=angular.module('githubApp',[]);
 
 githubApp.controller('githubCtrl',function($scope){
-	$scope.repository.name="Github API";
-	$scope.repository.credentials="mlabouardy";
+	$scope.repository="{\"name\":\"Github API\",\"credentials\":\"mlabouardy\"}";
 });
