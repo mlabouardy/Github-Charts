5c5682f50ef44d447986423bdf2d8929e2012597
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-16 14:55:35 +0200

    commits get

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ githubApp.controller('commitsCtrl',function($scope, $http){
 	$http.defaults.headers.common['Authorization'] = 'Basic ' + "mlabouardy" + ':' +"sinworm66125";
 	$http.get("https://api.github.com/repos/mlabouardy/AtelierCP/commits").success(function(data){
 		$scope.commits=data;
-	});
+	}).error(function () {
+        deferred.reject("Failed to login");
+    });
 });
 
 githubApp.controller('reposCtrl',function($scope, $http){
