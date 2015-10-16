cc139d32ec2b3f87cda9ea31556350a631fb9842
mlabouardy <mohamed.labouardy@gmail.com>
2015-10-16 14:32:23 +0200

    repos list

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ githubApp.controller('githubCtrl',function($scope){
 	$scope.name="Github API";
 	$scope.credentials="mlabouardy";
 });
+
+
+githubApp.controller('reposCtrl',function($scope, $http){
+	$http.get("https://api.github.com/users/mlabouardy/repos").success(function(data){
+		$scope.repos=data;
+	});
+});
